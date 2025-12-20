cacacc914b60ae743b945f4a27019d0ab6295d7d
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2025-12-20 23:10:15 +0530

    Fixed issue

--- src/app/layout/doc/components/doc-list/doc-list.component.ts
+++ src/app/layout/doc/components/doc-list/doc-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DocsService } from '../../services/docs.service';
 
 @Component({
@@ -9,7 +10,21 @@ import { DocsService } from '../../services/docs.service';
 export class DocListComponent implements OnInit {
     docs$ = this.docsService.docs$;
 
-    constructor(private docsService: DocsService) { }
+    constructor(
+        private docsService: DocsService,
+        private router: Router
+    ) { }
 
     ngOnInit(): void { }
+
+    onCreateNewDoc(): void {
+        // Create a new document with default values
+        this.docsService.createDoc({
+            title: 'Untitled',
+            blocks: []
+        }).subscribe(newDoc => {
+            // Navigate to the new document editor
+            this.router.navigate(['/docs', newDoc.id]);
+        });
+    }
 }
